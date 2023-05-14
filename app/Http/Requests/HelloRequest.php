1a04982c1a8deb37327f9dc3a36256d90b1727d6
draobfrus <aelfci5am@gmail.com>
2023-05-14 15:13:18 +0900

    Change:バリデーションの書き方を変更

--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -28,10 +28,20 @@ class HelloRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric|between:0,150',//
-        ];
+        // return [
+        //     'name' => 'required',
+        //     'mail' => 'email',
+        //     'age' => 'numeric|between:0,150',//
+        // ];
+    }
+
+    public function messages()
+    {
+        // return [
+        //     'name.required' => '名前は必ず入力してください。',
+        //     'mail.email' => 'メールアドレスが必要です。',
+        //     'age.numeric' => '年齢を整数で記入してください。',
+        //     'age.between' => '年齢は0〜150の間で入力してください。',
+        // ];
     }
 }
