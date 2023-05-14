47c7686b3148fd1efa022572731a5baa80c6c21c
draobfrus <aelfci5am@gmail.com>
2023-05-14 20:53:08 +0900

    Add:peopleテーブル内データの表示機能を作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\HelloRequest;
 use Validator;
 
@@ -11,24 +12,17 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->hasCookie('msg'))
+        if (isset($request->id))
         {
-            $msg = 'Cookie: ' . $request->cookie('msg');
+            $param = ['id' => $request->id];
+            $items = DB::select('select * from people where id = :id', $param);
         } else {
-            $msg = '※クッキーはありません。';
+            $items = DB::select('select * from people');
         }
-        return view('hello.index', ['msg'=> $msg]);
+        return view('hello.index', ['items'=> $items]);
     }//
 
     public function post(Request $request)
     {
-        $validate_rule = [
-            'msg' => 'required',
-        ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = response()->view('hello.index', ['msg'=>'「' . $msg . '」をクッキーに保存しました。']);
-        $response->cookie('msg', $msg, 100);
-        return $response;
     }//
 }
