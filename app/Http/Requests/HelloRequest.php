cbd6f6cef464fc9a731609059d603d052281fb4f
draobfrus <aelfci5am@gmail.com>
2023-05-14 15:58:58 +0900

    Change:オリジナルバリデータを用いてバリデーションを書き換え

--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -28,20 +28,20 @@ class HelloRequest extends FormRequest
      */
     public function rules()
     {
-        // return [
-        //     'name' => 'required',
-        //     'mail' => 'email',
-        //     'age' => 'numeric|between:0,150',//
-        // ];
+        return [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|hello',//
+        ];
     }
 
     public function messages()
     {
-        // return [
-        //     'name.required' => '名前は必ず入力してください。',
-        //     'mail.email' => 'メールアドレスが必要です。',
-        //     'age.numeric' => '年齢を整数で記入してください。',
-        //     'age.between' => '年齢は0〜150の間で入力してください。',
-        // ];
+        return [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入してください。',
+            'age.hello' => 'Hello! 入力は偶数のみ受け付けます。',
+        ];
     }
 }
