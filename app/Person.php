a037dcad4f4d3b7eec43fc5ff9d3a78e5458c575
draobfrus <aelfci5am@gmail.com>
2023-05-15 13:35:30 +0900

    Change:名前検索機能をローカルスコープを用いて書き換え

--- app/Person.php
+++ app/Person.php
@@ -9,5 +9,10 @@ class Person extends Model
     public function getData()
     {
         return $this->id . ': ' . $this->name . '（' . $this->age . '）';
-    } //
+    }
+
+    public function scopeNameEqual($query, $str)
+    {
+        return $query->where('name', $str);
+    }
 }
