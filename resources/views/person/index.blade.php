6439fadf2eb2002dd416973712ff60ddccad99ae
draobfrus <aelfci5am@gmail.com>
2023-05-15 11:49:37 +0900

    Add:Eloquentを使用してpeopleテーブルの一覧表示機能を作成

---
+++ resources/views/person/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <table>
+        @csrf
+        <tr><th>Name</th></tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{ $item->getData() }}</td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
+
+@section('footer')
+    @copyright 2020 tuyano.
+@endsection
